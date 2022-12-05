ae6109ae8f87ceadc883c5d3ed9fc53353d5744a
sabbir1020 <gazisabbir924@gamil.com>
2022-12-06 00:24:47 +0600

    data updated

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const courses = require("./data/course.json");
 app.get("/", (req, res) => {
   res.send("hello world");
 });
+app.get("/courses/:id", (req, res) => {
+  // console.log(req);
+  const id = req.params.id;
+  const course = courses.find((course) => course._id === id);
+  console.log(id);
+  res.send(course);
+});
 
 app.get("/course", (req, res) => {
   res.send(courses);
